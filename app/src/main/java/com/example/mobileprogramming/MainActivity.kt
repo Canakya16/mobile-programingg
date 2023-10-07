0039b003526bc2f02f859d99cc4a4bf0d872e67a
Canakya16 <canakyabima18@gmail.com>
2023-10-07 15:00:26 +0700

    explicit intent

--- app/src/main/java/com/example/mobileprogramming/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mobileprogramming
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -10,59 +11,21 @@ import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var inputPanjang: EditText
-    private lateinit var inputLebar: EditText
-    private lateinit var inputTinggi: EditText
-    private lateinit var result: TextView
-    private lateinit var btn: Button
+    private lateinit var btn_activity: Button
 
-    companion object {
-        private const val STATE_RESULT = "state_result"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        inputPanjang = findViewById(R.id.edt_length)
-        inputLebar = findViewById(R.id.edt_width)
-        inputTinggi = findViewById(R.id.edt_height)
-        btn = findViewById(R.id.btn_result)
-        result = findViewById(R.id.result)
-
-        btn.setOnClickListener(this)
-
-        if (savedInstanceState != null) {
-            val textResult = savedInstanceState.getString(STATE_RESULT)
-            result.text = textResult
+        btn_activity = findViewById(R.id.btn_activity)
+        btn_activity.setOnClickListener(this)
         }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, result.text.toString())
-    }
 
     override fun onClick(v: View?) {
-        if (v?.id == R.id.btn_result) {
-            val panjang = inputPanjang.text.toString().trim()
-            val lebar = inputLebar.text.toString().trim()
-            val tinggi = inputTinggi.text.toString().trim()
-            var isEmptyFields = false
-            if (panjang.isEmpty()) {
-                isEmptyFields = true
-                inputPanjang.error = "Field ini tidak boleh kosong"
-            }
-            if (lebar.isEmpty()) {
-                isEmptyFields = true
-                inputLebar.error = "Field ini tidak boleh kosong"
-            }
-            if (tinggi.isEmpty()) {
-                isEmptyFields = true
-                inputTinggi.error = "Field ini tidak boleh kosong"
-            }
-            if (!isEmptyFields) {
-                var hasil = panjang.toDouble() * lebar.toDouble() * tinggi.toDouble()
-                result.text = hasil.toString()
+        when (v?.id) {
+            R.id.btn_activity -> {
+                val moveIntent = Intent(this@MainActivity, MainActivity2::class.java)
+                startActivity(moveIntent)
             }
         }
     }
---
+++ app/src/main/java/com/example/mobileprogramming/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.mobileprogramming
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    fun onClick(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
